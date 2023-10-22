dee2fd7032e3acee6ea81a1270b134c1578ff573
Eduardo Fonseca <66074930+EduardFonseca@users.noreply.github.com>
2023-10-22 18:16:21 +0100

    atualizcoes ate o item f

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,12 @@ uint8_t prevSup, prevSdown;
 // uint8_t LED_1 = 1, LED_2 = 1;
 
 // Our finite state machines
-fsm_t fsm1, fsm2, fsm3;
+fsm_t fsm1, // countdown state machine
+      fsm2, // blink pause and stop
+      fsm3, // config mode state machine
+      fsm4, // second half blink
+      fsm5, // control config example led
+      fsm6; // auxiliar state machine
 
 unsigned long blink_period = 500;
 int blink_var = 0; // se variavel for 0 maquina 2 deve estar em standby se for 1 ela entra em funcionameto
@@ -47,9 +52,21 @@ unsigned long press_time = 3000;
 int config_mode = 0;
 int config_led = 0;
 
-uint16_t count_time = 10; //seconds
-uint16_t count_ms = count_time * 1000; //miliseconds
-uint16_t t_led = count_ms/n_led; //miliseconds
+uint16_t t_led = 2000; //miliseconds
+
+int possible_times[4] = {1000, 2000, 5000, 10000};
+
+// violet, blue, cyan, green, yellow, orange or white
+int led_color = 0;
+int colors[7][3] = {{127, 0, 255}, {0,0,255},{0, 255, 255},{0,255,0},{255,255,0},{255,165,0},{255,255,255}};
+int color[3] = {colors[led_color][0],colors[led_color][1],colors[led_color][2]};
+
+char index_time = 1;
+float percent;
+int fsm4_flag = 0;
+int fsm6_flag = 0;
+int led_mode = 0;
+unsigned long time_buffer = 0;
 
 // inicializacao da strip
 NeoPixelConnect strip(LED_PIN, MAXIMUM_NUM_NEOPIXELS, pio0, 0);
@@ -64,6 +81,54 @@ void set_state(fsm_t& fsm, int new_state)
     fsm.tis = 0;
   }
 }
+
+void led_effect(int mode, unsigned long time){
+  if(mode == 0){
+    // turn on the right pixels   
+    for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+      if(i<=led_on){
+        strip.neoPixelSetValue(i, color[0],color[1], color[2]);  
+      }else{
+        strip.neoPixelSetValue(i, 0, 0, 0);  
+      }     
+    }
+  }else if (mode == 1){
+    // for the second half of its interval the LED must blink
+    if(((5-led_on)*t_led- time) <= t_led/2){
+      // state machine 4
+      if(fsm4.state == 0){
+        fsm4_flag = 1;
+      }
+    }else{
+        fsm4_flag = 0;
+    }
+  }else if(mode == 2){
+    // fades out from 100% to 0% over time
+    percent = ((5-led_on)*(float)t_led - (float)time)/(float)t_led;
+    for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+      if(i<led_on){
+        strip.neoPixelSetValue(i, color[0],color[1], color[2]);  
+      }else if(i==led_on){
+        strip.neoPixelSetValue(i, percent*color[0],percent*color[1], percent*color[2]);  
+      }else{
+        strip.neoPixelSetValue(i, 0, 0, 0);  
+      }     
+    }
+  }
+}
+
+
+// APAGAR
+void config_leds(int led){
+    for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+    if(i == led){
+      strip.neoPixelSetValue(i, 25, 25, 0);  
+    } else{
+      strip.neoPixelSetValue(i, 0, 0, 0);
+    }
+  }
+}
+
 
 
 void setup() 
@@ -82,6 +147,9 @@ void setup()
   set_state(fsm1, 0);
   set_state(fsm2, 0);    
   set_state(fsm3, 0);    
+  set_state(fsm4, 0);    
+  set_state(fsm5, 0);    
+  set_state(fsm6, 0);    
 }
 
 void loop() 
@@ -116,6 +184,9 @@ void loop()
       fsm1.tis = cur_time - fsm1.tes;
       fsm2.tis = cur_time - fsm2.tes; 
       fsm3.tis = cur_time - fsm3.tes; 
+      fsm4.tis = cur_time - fsm4.tes; 
+      fsm5.tis = cur_time - fsm5.tes;
+      fsm6.tis = cur_time - fsm6.tes;
 
       // TODO: ARRUMAR O ESTADO DE CONFIGURACAO
       // Calculate state transitions for the first state machine
@@ -157,9 +228,9 @@ void loop()
       // state0 = standby
       // state1 = led_on
       // state2 = led_off
-      if(fsm2.state==0 && (blink_var == 1 || config_mode == 1)){
+      if(fsm2.state==0 && blink_var == 1){
         fsm2.new_state = 1;
-      }else if(blink_var == 0 && config_mode == 0){
+      }else if(blink_var == 0){
         fsm2.new_state = 0;
       }else if(fsm2.state == 1 && fsm2.tis >= blink_period){
         fsm2.new_state = 2;
@@ -170,22 +241,80 @@ void loop()
       // Calculate state transitions for the third state machine
       // state0 = standby
       // state1 = button pressed
-      // state2 = config mode
-      if(fsm1.state==0){
-        if(Sup && !prevSup && fsm3.state != 1){
-          config_led += 1;
-          if(config_led > n_led-1){
-            config_led = 0;
-          }
-          fsm3.new_state = 1;
-          fsm3.new_state = 1;
-        }else if(!Sup && fsm3.state == 1){
-          fsm3.new_state = fsm3.prev_state;
-        }else if(fsm3.state == 1 && fsm3.tis >= press_time && fsm3.prev_state == 0){
-          fsm3.new_state = 2;
-        }else if(fsm3.state == 1 && fsm3.tis >= press_time && fsm3.prev_state != 0){
-          fsm3.new_state = 0;
+      // state2 = time config mode
+      // state3 = counting efect config mode
+      // state4 = colour config mode
+      if(Sup && !prevSup && fsm3.state != 1){
+        config_led += 1;
+        if(config_led > n_led-3){
+          config_led = 0;
         }
+        fsm3.new_state = 1;
+      }else if(!Sup && fsm3.state == 1){
+        if(fsm3.prev_state == 0){
+          fsm3.new_state = fsm3.prev_state; 
+        }else{
+          fsm3.new_state = fsm3.prev_state+1;
+          if(fsm3.prev_state == 4){
+            fsm3.new_state = 2;
+          }
+        }
+      }else if(fsm3.state == 1 && fsm3.tis >= press_time && fsm3.prev_state == 0){
+        fsm3.new_state = 2;
+      }else if(fsm3.state == 1 && fsm3.tis >= press_time && fsm3.prev_state != 0){
+        fsm3.new_state = 0;
+      }
+
+      // Calculate state transitions for the fourth state machine
+      // state0 = standby
+      // state1 = led_off
+      // state2 = led_on
+      if(fsm4.state == 0 && fsm4_flag == 1){
+        fsm4.new_state = 1;
+      }else if(fsm4.state == 1 && fsm4.tis >= t_led/16){
+        fsm4.new_state = 2;
+      }else if(fsm4.state == 2 && fsm4.tis >= t_led/16){
+        fsm4.new_state = 1;
+      }else if(fsm4_flag == 0){
+        fsm4.new_state = 0;
+      }
+
+      // Calculate state transitions for the fifth state machine
+      if(config_mode == 0){
+        fsm5.new_state = 0;
+      }else if(fsm5.state == 0 && config_mode == 1){
+        fsm5.new_state = 1;
+      }else if(fsm3.state != 0 && fsm3.state != 1 && fsm5.prev_state != fsm3.state-1 && fsm5.state != fsm3.state-1){
+        Serial.print("teste");
+        fsm5.new_state = fsm3.state-1;
+      }else if(fsm5.state == 1 && fsm5.tis >= t_led){
+        fsm5.new_state = 11;
+      }else if(fsm5.state == 11 && fsm5.tis >= 1000){
+        fsm5.new_state = 1;
+      }else if(fsm5.state == 2 && (led_mode == 0 || led_mode == 2) && fsm5.tis >= 2000){ 
+        fsm5.new_state = 21;
+      }else if(fsm5.state == 21 && fsm5.tis >= 1000){
+        fsm5.new_state = 2;
+      }
+
+      // Calculate state transitions for the sixth state machine
+      if(fsm6_flag == 0){
+        fsm6.new_state = 0;
+      }else if(fsm6.state == 0 && fsm6_flag == 1){
+        fsm6.new_state = 1;
+      }else if(fsm6.state == 1 && fsm6.tis >= 2000/2){
+        time_buffer+=fsm6.tis;
+        fsm6.new_state = 2;
+      }else if(fsm6.state==2 && fsm6.tis >= 2000/16){
+        time_buffer+=fsm6.tis;
+        fsm6.new_state = 3;
+      }else if(fsm6.state==3 && fsm6.tis >= 2000/16){
+        time_buffer+=fsm6.tis;
+        fsm6.new_state = 2;
+      }else if(time_buffer > 2000){
+        fsm6.new_state = 4;
+      }else if(fsm6.state == 4 && fsm6.tis >= 1000){
+        fsm6.new_state = 1;
       }
 
       // Calculate next state for the first state machine
@@ -207,30 +336,23 @@ void loop()
         
         // Add LED IF Sup is pressed
         if(Sup && !prevSup && led_on < n_led-1){
-          Serial.print("Sup pressed");
           led_on+=1;
           led_added+=1;
         }
 
-        // if time elapsed and time in stae is grater time for the led to be on
-        if(time_elapsed+fsm1.tis-(led_added*t_led) >= (5-led_on)*t_led){
-          //Turn off the led
-          led_on-=1;
-        }
-        
         if(fsm1.new_state==3){
           // if paused update time elapsed
           time_elapsed += fsm1.tis;
         }
 
-        // turn on the right pixels  
-        for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
-          if(i<=led_on){
-            strip.neoPixelSetValue(i, 25, 25, 0);  
-          }else{
-            strip.neoPixelSetValue(i, 0, 0, 0);  
-          }     
+        // if time elapsed and time in stae is grater time for the led to be on
+        // TODO: 
+        if(time_elapsed+fsm1.tis-(led_added*t_led) >= (5-led_on)*t_led){
+          //Turn off the led
+          led_on-=1;
         }
+        // turn on the right pixels  
+        led_effect(led_mode, time_elapsed+fsm1.tis-(led_added*t_led));
 
         }else if(fsm1.state == 2){
         //finished countdown
@@ -257,8 +379,10 @@ void loop()
       }
       
       // Calculate next state for the second state machine
+      // TODO: Para o exercicio e alterar essa maquina
       if(fsm2.state == 0){
         //Show LED 
+        // funcao (tipo mostragem)
         strip.neoPixelShow();
       }else if(fsm2.state == 1){
         //Show LED 
@@ -275,54 +399,194 @@ void loop()
         config_mode = 0;
       }else if(fsm3.state==1){
         // wait for time
-        config_mode = 1;
       }else if(fsm3.state==2){
         config_mode = 1;
+        fsm1.new_state = 0;
+        blink_var = 0;
         // turn on the right pixels
-        for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
-          if(i == config_led){
-            strip.neoPixelSetValue(i, 25, 25, 0);  
-          } else{
-            strip.neoPixelSetValue(i, 0, 0, 0);
+        if(Sdown && !prevSdown){
+          if(index_time == 3){
+            index_time = 0;
+          }else{
+            index_time += 1;
+          }
+          t_led = possible_times[index_time];
+        }
+
+        // config_leds(fsm3.state-2);
+      }else if(fsm3.state == 3){
+        config_mode = 1;
+        fsm1.new_state = 0;
+        // turn on the right pixels
+        if(Sdown && !prevSdown){
+          if(led_mode == 2){
+            led_mode = 0;
+          }else{
+            led_mode += 1;
           }
         }
-      } 
 
+        // config_leds(fsm3.state-2);
+      }else if(fsm3.state == 4){
+        if(Sdown && !prevSdown){
+          led_color +=1;
+          if(led_color >= 7){
+            led_color =0;
+          }
+        }
+
+        color[0] = colors[led_color][0];
+        color[1] = colors[led_color][1];
+        color[2] = colors[led_color][2];
+        // config_leds(fsm3.state-2);
+      }
+
+      // Calculate next state for the fourth state machine
+      if(config_mode == 0){
+        if(fsm4.state == 0){
+          for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+            if(i<=led_on){
+              strip.neoPixelSetValue(i, color[0],color[1], color[2]);  
+            }else{
+              strip.neoPixelSetValue(i, 0, 0, 0);  
+            }     
+          }
+        }else if(fsm4.state == 1){
+          for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+            if(i<led_on){
+              strip.neoPixelSetValue(i, color[0],color[1], color[2]);  
+            }else{
+              strip.neoPixelSetValue(i, 0, 0, 0);  
+            }     
+          }  
+        }else if(fsm4.state == 2){
+          for(uint8_t i = 0; i < MAXIMUM_NUM_NEOPIXELS; i++) {
+            if(i<=led_on){
+              strip.neoPixelSetValue(i, color[0],color[1], color[2]);  
+            }else{
+              strip.neoPixelSetValue(i, 0, 0, 0);  
+            }     
+          }  
+        }
+      }
+
+      // Calculate next state for the fifth state machine
+      if(fsm5.state == 0){
+        // standby
+        fsm6_flag = 0;
+      }else if(fsm5.state == 1){
+        // turn on pixels 0 and 4 (led_config and led_exmp)
+        fsm6_flag = 0;
+        strip.neoPixelFill(0,0,0);
+        strip.neoPixelSetValue(config_led, 25, 25, 0);
+        strip.neoPixelSetValue(4, 25, 25, 0);
+      }else if(fsm5.state == 11){
+        // turn on pixels 0 and 5 (led_config and led_exmp)
+        fsm6_flag = 0;
+        strip.neoPixelFill(0,0,0);
+        strip.neoPixelSetValue(config_led, 25, 25, 0);
+      }else if(fsm5.state == 2){
+        strip.neoPixelFill(0,0,0);
+        strip.neoPixelSetValue(config_led, 25, 25, 0);
+        if(led_mode == 0){
+          fsm6_flag = 0;
+          strip.neoPixelSetValue(4, 25, 25, 0);
+        }if(led_mode ==1){
+          fsm6_flag = 1;
+        }if(led_mode == 2){
+          fsm6_flag = 0;
+          if(fsm5.tis > 2000){
+            percent = 0;
+          }else{
+            percent = (float)(2000-fsm5.tis)/(float)(2000);
+          }
+          strip.neoPixelSetValue(4, percent*25,percent*25, 0);
+        }
+      }else if(fsm5.state == 3){
+        fsm6_flag = 0;
+        strip.neoPixelFill(0,0,0);
+        strip.neoPixelSetValue(config_led, 25, 25, 0);
+        strip.neoPixelSetValue(4, color[0],color[1],color[2]);
+      }else if(fsm5.state == 21){
+        fsm6_flag = 0;
+        strip.neoPixelFill(0,0,0);
+        strip.neoPixelSetValue(config_led, 25, 25, 0);
+      }
+
+      // Calculate next state for the sixth state machine
+      if(fsm6.state == 0){
+        time_buffer = 0;
+      }else if(fsm6.state == 1){
+        strip.neoPixelSetValue(4, 25, 25, 0);
+      }else if(fsm6.state == 2){
+        // led apagado
+      }else if(fsm6.state == 3){
+        strip.neoPixelSetValue(4, 25, 25, 0);
+      }else if(fsm6.state == 4){
+        time_buffer = 0;
+      }
 
       // Update the states
       set_state(fsm1, fsm1.new_state);
       set_state(fsm2, fsm2.new_state);
       set_state(fsm3, fsm3.new_state);
+      set_state(fsm4, fsm4.new_state);
+      set_state(fsm5, fsm5.new_state);
+      set_state(fsm6, fsm6.new_state);
 
+      // // Debug using the serial port
+      // Serial.print("Sgo: ");
+      // Serial.print(Sgo);
+      // Serial.print(" Sup: ");
+      // Serial.print(Sup);
+      // Serial.print(" Sdown: ");
+      // Serial.print(Sdown);
 
-      // Debug using the serial port
-      Serial.print("Sgo: ");
-      Serial.print(Sgo);
-      Serial.print(" Sup: ");
-      Serial.print(Sup);
-      Serial.print(" Sdown: ");
-      Serial.print(Sdown);
-
-      Serial.print(" fsm1.state: ");
-      Serial.print(fsm1.state);
+      // Serial.print(" fsm1.state: ");
+      // Serial.print(fsm1.state);
       
-      Serial.print(" fsm1.tis: ");
-      Serial.print(fsm1.tis);
+      // Serial.print(" fsm1.tis: ");
+      // Serial.print(fsm1.tis);
 
-      Serial.print(" fsm2.state: ");
-      Serial.print(fsm2.state);
+      // Serial.print(" fsm2.state: ");
+      // Serial.print(fsm2.state);
       
-      Serial.print(" fsm2.tis: ");
-      Serial.print(fsm2.tis);
+      // Serial.print(" fsm2.tis: ");
+      // Serial.print(fsm2.tis);
 
-      Serial.print(" fsm3.state: ");
-      Serial.print(fsm3.state);
+      // Serial.print(" fsm3.state: ");
+      // Serial.print(fsm3.state);
 
-      Serial.print(" fsm3.tis: ");
-      Serial.print(fsm3.tis);
+      // Serial.print(" fsm3.tis: ");
+      // Serial.print(fsm3.tis);
 
-      Serial.print(" blink_var: ");
-      Serial.print(blink_var);
+      // Serial.print(" fsm4.state: ");
+      // Serial.print(fsm4.state);
+
+      // Serial.print(" fsm4_flag: ");
+      // Serial.print(fsm4_flag);
+
+      Serial.print(" fsm5.state: ");
+      Serial.print(fsm5.state);
+
+      Serial.print(" fsm5.tis: ");
+      Serial.print(fsm5.tis);
+
+      Serial.print(" fsm5.prev_state: ");
+      Serial.print(fsm5.prev_state);
+
+      Serial.print(" t_led: ");
+      Serial.print(t_led);
+
+      Serial.print(" led_mode: ");
+      Serial.print(led_mode);
+
+      Serial.print(" time_elapsed: ");
+      Serial.print(time_elapsed);
+
+      Serial.print(" time_buffer: ");
+      Serial.print(time_buffer);
+      
 
       Serial.println();
 
